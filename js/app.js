cbd66dc1b13520998c29d5993f223d44df9bd0c8
Stefan Haslinger <stefan.haslinger@mittenin.at>
2014-06-03 17:45:52 +0200

    ausgabe fixture vertrag

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 App = Ember.Application.create();
+App.ApplicationAdapter = DS.FixtureAdapter;
+// App.ApplicationAdapter = DS.ActiveModelAdapter;
 
 App.Router.map(function() {
   // put your routes here
@@ -6,6 +8,38 @@ App.Router.map(function() {
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return this.store.find('contract');
   }
 });
+
+App.IndexController = Ember.ArrayController.extend({});
+
+App.Contract = DS.Model.extend({
+  runtime: DS.attr('number'),
+  startdate: DS.attr('date'),
+  created_at: DS.attr('string'),
+  updated_at: DS.attr('string'),
+
+  formattedCreatedAt: function() {
+    moment.lang('de');
+    return moment(this.get('created_at')).format("LLL");
+  }.property('created_at'),
+
+  formattedUpdatedAt: function() {
+    moment.lang('de');
+    return moment(this.get('updated_at')).format("LLL");
+  }.property('updated_at'),
+
+  formattedStartdate: function() {
+    moment.lang('de');
+    return moment(this.get('startdate')).format("LL");
+  }.property('startdate')
+});
+
+App.Contract.FIXTURES = [{
+  id: 1,
+  runtime: 12,
+  startdate: new Date(2014, 06, 02),
+  created_at: new Date(2014, 06, 03, 12, 00, 00),
+  updated_at: new Date(2014, 06, 03, 13, 12, 00)
+}];
